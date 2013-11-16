5651631c23f5173f2056ff7248fa21cdfadd67c2
will chan <willchan2@gmail.com>
2013-11-16 15:05:36 -0500

    renamed specs and the tests html file

---
+++ tests/spec/jqdropdown-spec.js
@@ -0,0 +1,23 @@
+describe('jQdropdown', function() {
+  it('extends jQuery.fn', function() {
+    expect(jQuery.fn.jQdropdown).toBeDefined();
+    expect(jQuery('<div />').jQdropdown).toBeDefined();
+  });
+});
+
+describe('options that jQdropdown can take', function() {
+  it('throws an Invalid Argument error when dropdownSelector is not specified in the options passed to jQdropdown', function() {
+    expect(function() {
+      jQuery('<div />').jQdropdown();
+    }).toThrow(new Error('Invalid Argument: Please supply either a dropdownClass or a dropdownId for the dropdown selector'));
+  });
+
+  it('throws an Invalid Argument error when actions is provided and is not an array of HTML tags', function() {
+    expect(function() {
+      jQuery('<div />').jQdropdown({
+        dropdownClass: 'someClass',
+        actions: 'testAction'
+      }).appendTo('body').trigger('click');
+    }).toThrow(new Error('Invalid Arguments: actions; must be an array of html elements'));
+  });
+});
